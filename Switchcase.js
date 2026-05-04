617ca495d460cd3ed9be19179d29ef6adcbfe708
Utkuri Vignesh <vigneshutkuri@gmail.com>
2026-05-04 14:28:53 +0530

    switching

---
+++ Switchcase.js
@@ -0,0 +1,19 @@
+let a=5;
+let b=10;
+let v='+';
+switch(v){
+    case '+':
+        console.log(a+b);   
+        break;
+    case '-':
+        console.log(a-b);    
+        break;
+    case '*':
+        console.log(a*b);   
+        break;  
+    case '/':
+        console.log(a/b);    
+        break;          
+    default:
+        console.log("Invalid operator");
+}
